5d92c24a308093810bb56cfe28f7bf565c8aa521
vipink18 <65859121+vipink18@users.noreply.github.com>
2022-09-27 09:17:41 +0530

    Update SupplierController.java

--- FarmAssist-1/src/main/java/com/cg/farm/controller/SupplierController.java
+++ FarmAssist-1/src/main/java/com/cg/farm/controller/SupplierController.java
@@ -9,14 +9,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cg.farm.entity.Farmer;
 import com.cg.farm.entity.Supplier;
-import com.cg.farm.exception.FarmerNotFoundException;
 import com.cg.farm.exception.SupplierNotFoundException;
-import com.cg.farm.service.FarmerServiceImpl;
 import com.cg.farm.service.SupplierServiceImpl;
 
 @RestController
@@ -52,6 +50,6 @@ public class SupplierController {
 	}
 	
 	
-	
 
 }
+
